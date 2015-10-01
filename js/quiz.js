bfc9fcb824693ffdb6dd47d7b6e551e1c63a6bac
PatrickJRex <prexdesigns@gmail.com>
2015-10-01 00:35:22 -0400

    Here's the stuff

---
+++ js/quiz.js
@@ -0,0 +1,27 @@
+var responses = [
+    {
+    title:'You\'re a Glam Mombie',
+    img:'http://placeimg.com/1800/600/arch',
+    feed:'https://google.com',
+    shareImg:'http://placeimg.com/1800/600/animals'    
+    },
+     {
+    title:'You\'re a Soccer Mombie',
+    img:'http://placeimg.com/1800/600/animals',
+    feed:'https://google.com' ,
+    shareImg:'http://placeimg.com/1800/600/animals'      
+    },
+     {
+    title:'You\'re a Last Minute Mombie',
+    img:'http://placeimg.com/1800/600/nature',
+    feed:'https://google.com',
+    shareImg:'http://placeimg.com/1800/600/animals'      
+    }
+    
+]
+
+var total = 0;
+
+
+
+
